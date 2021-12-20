bcdde213566b96cb9efb289214cdc038ad9cc141
Halakh96 <halakhaled96@hotmail.com>
2021-12-21 00:20:07 +0300

    done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const AllCategories = document.querySelectorAll(".Category-Title");
 
+//  news api
 const fetchCat =(event)=>{
 let cat = event.target.getAttribute("data-value")
 console.log(cat);
@@ -10,9 +11,8 @@ for(let i=0;i<AllCategories.length;i++){
 
 }
 
-
 const NewsFunction = (cat) => {
-    url=`https://newsapi.org/v2/top-headlines?category=${cat}&apiKey=233b6411d34640098f2872adb12f88d8`
+    url=`https://newsapi.org/v2/top-headlines?category=${cat}&language=en&apiKey=233b6411d34640098f2872adb12f88d8`
 
     fetch(url).then((res=>{
         res.json().then((res)=>{
@@ -37,3 +37,45 @@ document.getElementById('newsCard').innerHTML = res.articles.map(item =>
     }))
 
 }
+
+
+
+// weather api
+
+
+const weatherFetch =(city)=>{
+url=`https://api.weatherbit.io/v2.0/current?city=${city}&KEY=72b065a0dd03481089c7c4cffaf9e566`
+fetch(url).then((res=>{
+    res.json().then((res)=>{
+        console.log(res.data)
+ //mapping here
+  
+document.getElementById('weather').innerHTML = res.data.map(item =>
+`<div class="container-fluid p-5 ">
+    <div class="d-flex justify-content-evenly ">
+<div class="fw-bold fs-2 fst-italic ">${item.city_name}</div>
+<div> ${item.weather.icon}</div>
+</div>
+
+<div class=" text-center m-3">
+<h1>${item.temp}&deg</h1>
+</div>
+<div class="text-center m-3">${item.weather.description}</div>
+<div class="d-flex justify-content-evenly ">
+<div>Humidity:${item.rh}%</div>
+<div>Wind Speed: ${item.wind_spd}km/h</div>
+</div>
+</div>`
+    ).join('')
+    })
+}))
+
+
+}
+
+
+const city = document.querySelector("#cityText");
+const searchBtn = document.querySelector("#searchBtn");
+
+searchBtn.addEventListener("click",weatherFetch(city));
+
