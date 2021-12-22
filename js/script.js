fb2795216c52dd155de664a388750834c3e9159f
Halakh96 <halakhaled96@hotmail.com>
2021-12-22 10:28:35 +0300

    done

--- js/script.js
+++ js/script.js
@@ -1,84 +1,91 @@
+
+// API NEWS
 const AllCategories = document.querySelectorAll(".Category-Title");
 
-//  news api
 const fetchCat =(event)=>{
-let cat = event.target.getAttribute("data-value")
-console.log(cat);
-NewsFunction(cat);
-}
+    let cat = event.target.getAttribute("data-value")
+    console.log(cat);
+    NewsFunction(cat);
+    }
 for(let i=0;i<AllCategories.length;i++){
     AllCategories[i].addEventListener("click",fetchCat);
 
 }
 
 const NewsFunction = (cat) => {
-    url=`https://newsapi.org/v2/top-headlines?category=${cat}&language=en&apiKey=233b6411d34640098f2872adb12f88d8`
+    url=`https://newsapi.org/v2/top-headlines?category=${cat}&language=en&pageSize=10&apiKey=233b6411d34640098f2872adb12f88d8`
 
-    fetch(url).then((res=>{
-        res.json().then((res)=>{
+    fetch(url)
+    .then((res=>{res.json()
+        .then((res)=>{ 
             console.log(res.articles)
     
             //mapping here
       
 document.getElementById('newsCard').innerHTML = res.articles.map(item =>
-`<div class="card">
-      
-      <div class="col-12"><img class="w-100 h-100 "src="${item.urlToImage}" alt=""></img>
-      </div>
-      <div class="col-12">
-          <a href=${item.url}><h4>${item.title}</h4></a>
-          <p>${item.description}</p>
-      </div>
-      <div class="col-12">
-           <p>${item.publishedAt} </p>
-      </div>
-          </div>`).join('')
+`<div class="card p-3 mt-3">
+<div class="row ">
+<div class="col-12 col-md-4 "><img class="w-100 h-100 "src="${item.urlToImage}" alt=""></img>
+</div>
+<div class="col-12 col-md-8 mt-2">
+    <h4>${item.title}</h4>
+    <p>${item.description}</p>
+    <a href="${item.url}">Read More</a>
+    <p class="float-end">${item.publishedAt} </p>
+</div>
+    </div>
+    </div>
+    `
+          ).join('')
 
         })
     }))
 
 }
 
-/*SEARCH BY USING A CITY NAME (e.g. athens) OR A COMMA-SEPARATED CITY NAME ALONG WITH THE COUNTRY CODE (e.g. athens,gr)*/
-const form = document.querySelector("#weather form");
-const input = document.querySelector("#weather input");
-const msg = document.querySelector("#weather .msg");
-const city = document.querySelector("#weather .city");
-/*PUT YOUR OWN KEY HERE - THIS MIGHT NOT WORK
-SUBSCRIBE HERE: https://home.openweathermap.org/users/sign_up*/
-const apiKey = "3ee75691029a24f2dc485291c42637ea";
 
-form.addEventListener("submit", e => {
-  e.preventDefault();
-  const inputVal = input.value;
-  //ajax here
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
 
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      const { main, name, sys, weather } = data;
-      const icon = `https://openweathermap.org/img/wn/${
-        weather[0]["icon"]
-      }@2x.png`;
-      const markup = `
-      <div class="m-2 p-4 ">
-        <h2 class="city-name" data-name="${name},${sys.country}">
-          <span>${name}</span>
-          <sup>${sys.country}</sup>
-        </h2>
-        <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
-        <figure>
-          <img class="city-icon" src=${icon} alt=${weather[0]["main"]}>
-          <figcaption class="mt-3">${weather[0]["description"]}</figcaption>
-        </figure>
-        </div>
-      `;
-      city.innerHTML = markup;
-    })
-    
+// Api Weather
+const api = '3ee75691029a24f2dc485291c42637ea'; 
 
-  msg.textContent = "";
-  form.reset();
-  input.focus();
+const iconImg = document.getElementById('weather-icon');
+const loc = document.querySelector('#location');
+const tempC = document.querySelector('.c');
+const desc = document.querySelector('.desc');
+
+
+window.addEventListener('load', () => {
+  let long;
+  let lat;
+  // Accesing Geolocation of User
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      // Storing Longitude and Latitude in variables
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+      const base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api}&units=metric`;
+
+      // Using fetch to get data
+      fetch(base)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          const { temp  } = data.main;
+          const place = data.name;
+          const { description, icon } = data.weather[0];
+
+          const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;          
+
+          // Interacting with DOM to show data
+          iconImg.src = iconUrl;
+          loc.textContent = `${place}`;
+          desc.textContent = `${description}`;
+          tempC.textContent = `${Math.floor(temp)}°`;
+
+          
+         
+        });
+    });
+  }
 });
